8a75bd4851e297a17e2a9afb3cb7a3fcb09a68d1
sanketbijawe <ssbijawe@tekion.com>
2023-08-27 17:42:15 +0530

    Refactored domain layer interface

--- domain/domain.go
+++ domain/domain.go
@@ -1,5 +1,15 @@
 package domain
 
+import (
+	"context"
+	"todo_app_mux/models"
+)
+
 type IDomainService interface {
-	ITodoItem
+	AddItemToList(ctx context.Context, item string) error
+	GetTodoItems(ctx context.Context) ([]models.TodoListItem, error)
+	UpdateItemFromTodo(ctx context.Context, itemId, item string) error
+	DeleteItemFromTodo(ctx context.Context, itemId string) error
+	MarkItemComplete(ctx context.Context, itemId string) error
+	MarkItemIncomplete(ctx context.Context, itemId string) error
 }
